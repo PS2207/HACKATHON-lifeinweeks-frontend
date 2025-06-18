cdc015b8576add42ac9bf6a110a846111713d760
P Sinha <95397232+PS2207@users.noreply.github.com>
2025-06-18 15:55:45 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment ={
 // if we don't set a custom domain or project name of backend
 // Netify assign such name for frontent url like 'https://darling-eclair-693cf7.netlify.app/'
    // apiUrl : 'https://lifeinweeks-backend.onrender.com/api' // 'Backend Render cloud app URL' to connect with deployed backend
-    apiUrl : 'https://hackathone-backendapp-on-render.onrender.com/'
+    apiUrl : 'https://hackathone-backendapp-on-render.onrender.com/api'
 
 }
